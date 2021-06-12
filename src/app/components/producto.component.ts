d167a0fb11b9bfce7e9abd436bced6dfe3e42996
Jhonatan Mejia <quenandres@gmail.com>
2021-06-11 22:20:49 -0500

    Ciclo de vida de los componentes15/56

--- src/app/components/producto.component.ts
+++ src/app/components/producto.component.ts
@@ -1,4 +1,13 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, 
+    Input, 
+    Output, 
+    EventEmitter, 
+    OnChanges, 
+    SimpleChanges, 
+    OnInit, 
+    DoCheck, 
+    OnDestroy 
+} from '@angular/core';
 import { Product } from '../product.model';
 
 //Decoradores, dan contexto a angular para saber si es un componente o un pipe, etc.
@@ -6,9 +15,30 @@ import { Product } from '../product.model';
     selector: 'app-product',
     templateUrl: './product.component.html'
 })
-export class ProductComponent {
+export class ProductComponent implements OnInit, DoCheck, OnDestroy {
     @Input() product: Product;
     @Output() productClicked: EventEmitter<any> = new EventEmitter();
+
+    constructor() {
+        console.log('1. constructor');
+    }
+
+    /* ngOnChanges(changes: SimpleChanges) {
+        console.log('2. ngOnChanges');
+        console.log(changes);        
+    } */
+
+    ngOnInit() {
+        console.log('3. ngOnInit');
+    }
+
+    ngDoCheck() {
+        console.log('4. DoCheck');
+    }
+
+    ngOnDestroy() {
+        console.log('5. ngOnDestroy');
+    }
 
     addCart(){
         console.log('añadir al carrito');
