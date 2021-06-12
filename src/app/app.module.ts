7aa4e649f0ab44ddf8047646e266f9f38325be80
Jhonatan Mejia <quenandres@gmail.com>
2021-06-11 23:27:15 -0500

    Uso de ng generate y ng lint17/56

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ProductComponent } from './components/producto.component';
+import { CartComponent } from './cart/cart.component';
 @NgModule({
   declarations: [
     AppComponent,
-    ProductComponent
+    ProductComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
