51c16e4cce66f75de2d322add66e566b24f77f12
Jhonatan Mejia <quenandres@gmail.com>
2021-06-13 22:31:41 -0500

    Se crea un servicio para que traiga la informacion segun el parametro que se lo enviamos en un ruta especificando el nombre del parametro, hacemos una funcion que vaya al servicio y busque el registro que necesitamos. Creando el detalle de cada producto25/56

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { ProductsComponent } from './products/products.component';
 import { ContactComponent } from './contact/contact.component';
 import { DemoComponent } from './demo/demo.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
 
 const routes: Routes = [
   {
@@ -22,13 +23,17 @@ const routes: Routes = [
     component: ProductsComponent
   },
   {
+    path: 'products/:id',
+    component: ProductDetailComponent
+  },
+  {
     path: 'contact',
     component: ContactComponent
   },
   {
     path: 'demo',
     component: DemoComponent
-  },
+  },  
   {
     path: '**',
     component: PageNotFoundComponent
